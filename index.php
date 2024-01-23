dbc0dc74a7a4312d4f5feb15bd910da147afd999
MuhammedAnushahad <85872675+MuhammedAnushahad@users.noreply.github.com>
2024-01-23 11:07:47 +0530

    Update index.php
    jhggb

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>Index</title>
  </head>
  <body>
- <?php echo '<p>Hello world from PHP! INCIEM pipeline!!</p>'; ?> 
+ <?php echo '<p>Hello world from PHP! INCIEM !!</p>'; ?> 
  </body>
 </html>
